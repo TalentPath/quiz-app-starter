89f723352d43a1e96a469fe627c8eacae788928b
Josh Parada <joshparada@Joshs-MacBook-Pro.local>
2021-11-17 17:35:40 -0600

    updated files

--- questions.js
+++ questions.js
@@ -1,1 +1,45 @@
 //Your quiz questions array of objects goes here
+const questions = [
+    {
+        question: 'What is the solution of: 8×3+10÷2−4',
+        answers: [
+            { text: '25', correct: true },
+            { text: '-12', correct: false },
+            { text: '13', correct: false },
+            { text: '2', correct: false }
+        ]
+    },
+    {
+        question: 'What is the solution of: 24÷6−9+3×6',
+        answers: [
+            { text: '13', correct: true },
+            { text: '18', correct: false },
+            { text: '-18', correct: false },
+            { text: '-13', correct: false }
+        ]
+    }, {
+        question: 'What is the solution of: 5+3×2−1',
+        answers: [
+            { text: '10', correct: true },
+            { text: '15', correct: false },
+            { text: '8', correct: false },
+            { text: '20', correct: false }
+        ]
+    }, {
+        question: 'What is the solution of: 4+6×15÷6−2',
+        answers: [
+            { text: '17', correct: true },
+            { text: '24', correct: false },
+            { text: '5', correct: false },
+            { text: '14', correct: false }
+        ]
+    }, {
+        question: 'What is the solution of: 10÷5+5×2−6',
+        answers: [
+            { text: '6', correct: true },
+            { text: '-22', correct: false },
+            { text: '22', correct: false },
+            { text: '8', correct: false }
+        ]
+    }
+]
